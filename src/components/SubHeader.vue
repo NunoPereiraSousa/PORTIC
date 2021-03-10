bddc60a2a18707d16ae78a9100a03dca838b3c9c
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-10 12:21:14 +0000

    Subheader completed

--- src/components/SubHeader.vue
+++ src/components/SubHeader.vue
@@ -1,13 +1,26 @@
 <template>
-  <header class="subheader  flex flex-jc-sb flex-ai-c">
-    <div class="subheader__email flex flex-jc-sb flex-ai-c">
-      <i class="fa fa-envelope" aria-hidden="true"></i>
+  <header class="subheader grid">
+    <div class="subheader__contacts flex flex-jc-sb flex-ai-c">
+      <i class="fa fa-envelope"></i>
       <p>portic@portic.ipp.pt</p>
+
+      <div class="hide-for-mobile flex flex-jc-sb flex-ai-c">
+        <i class="fas fa-phone-volume"></i>
+        <p>(+351) 22 557 1020</p>
+      </div>
     </div>
-    <div class="subheader__socials flex flex-jc-sb flex-ai-c">
+    <div class="subheader__socials flex flex-jc-sb flex-ai-c hide-above-phone">
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-instagram"></i>
+      <i class="fab fa-flickr"></i>
       <i class="fab fa-youtube"></i>
       <i class="fab fa-linkedin"></i>
       <i class="fab fa-facebook-square"></i>
+    </div>
+    <div class="subheader__languages flex flex-jc-sb flex-ai-c">
+      <p>EN</p>
+      <hr />
+      <p class="selected">PT</p>
     </div>
   </header>
 </template>
