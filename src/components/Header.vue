da78c74970268894bb91521fad6a103ab7257b0f
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-10 13:01:42 +0000

    Mobile sidebar started

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,61 @@
 <template>
   <header>
-    <nav>
-      <a href=""><img src="../assets/logo.png" alt=""/></a>
-      <div class="header__hamburger hide-for-desktop">
-        <div class="header__hamburger__lines"></div>
-        <div class="header__hamburger__lines"></div>
-        <div class="header__hamburger__lines"></div>
+    <nav class="navbar flex flex-jc-sb flex-ai-c">
+      <a href="" class="navbar__logo"
+        ><img src="../assets/logo.png" alt="PORTIC"
+      /></a>
+      <div class="navbar__hamburger hide-for-desktop" @click="toggleNavbar">
+        <div class="navbar__hamburger__lines"></div>
+        <div class="navbar__hamburger__lines"></div>
+        <div class="navbar__hamburger__lines"></div>
       </div>
-      <!-- @click="toggleNavbar" -->
+      <div class="navbar__mobile_menu">
+        <div class="navbar__mobile_menu__info">
+          <h1>portic</h1>
+          <p>An open door towards the future</p>
+          <hr />
+        </div>
+        <div class="navbar__mobile_menu_links">
+          <a href="">Unities</a>
+          <a href="">Courses</a>
+          <a href="">Positions</a>
+          <a href="">Areas</a>
+          <a href="">Projects</a>
+          <a href="">Media</a>
+        </div>
+        <div class="navbar__mobile_menu_policy ">
+          <p>Privacy Policy & © Copy Right 2021</p>
+        </div>
+      </div>
     </nav>
+    <!-- <div class="navbar__mobile_overlay"></div> -->
   </header>
 </template>
 
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  methods: {
+    toggleNavbar() {
+      let lines = document.querySelectorAll(".navbar__hamburger__lines");
+
+      lines.forEach(line => {
+        line.classList.toggle("open");
+      });
+
+      // let title = document.querySelector(".header__title");
+      // title.classList.toggle("change__color");
+
+      let navbar__mobile_menu = document.querySelector(".navbar__mobile_menu");
+
+      navbar__mobile_menu.classList.toggle("open__overlay");
+
+      // let navbar__mobile_overlay = document.querySelector(
+      //   ".navbar__mobile_overlay"
+      // );
+      // navbar__mobile_overlay.classList.toggle("overlay_opened");
+    }
+  }
 };
 </script>
 
