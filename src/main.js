bfa707455507a239f22cf55897308716a55c2bc4
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-14 18:05:35 +0000

    Carousel working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
+import VueGlide from "vue-glide-js";
+import "vue-glide-js/dist/vue-glide.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+Vue.use(VueGlide);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -141,17 +141,60 @@
       <h1 class="testimonials__title">
         Clients who placed their trust in us
       </h1>
-      <div class="testimonials__cards grid">
-        <TestimonialCard
-          name="Alex Poporov"
-          company="Apple inc"
-          content="Lorem ipsum dolor sit amet, consectetur 
+      <div class="testimonials__cards">
+        <vue-glide
+          :gap="40"
+          :breakpoints="{
+            450: {
+              perView: 1
+            },
+            768: {
+              perView: 2
+            },
+            1200: {
+              perView: 3
+            }
+          }"
+          :rewind="false"
+          :bound="true"
+          :focusAt="'center'"
+        >
+          <vue-glide-slide v-for="i in 10" :key="i">
+            <TestimonialCard
+              name="Alex Poporov"
+              company="Apple inc"
+              content="Lorem ipsum dolor sit amet, consectetur 
                     adipiscing elit, sed do eiusmod tempor 
                     incididunt. Lorem ipsum dolor sit amet, 
                     adipiscing elit, sed do eiusmod tempor 
                     incididunt. Lorem ipsum dolor sit amet, 
                     adipiscing elit, sed do eiusmod tempor."
-        />
+          /></vue-glide-slide>
+          <template slot="control">
+            <button data-glide-dir="<">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 400.004 400.004"
+                class="left__arrow"
+              >
+                <path
+                  d="M382.688 182.686H59.116l77.209-77.214c6.764-6.76 6.764-17.726 0-24.485-6.764-6.764-17.73-6.764-24.484 0L5.073 187.757c-6.764 6.76-6.764 17.727 0 24.485l106.768 106.775a17.252 17.252 0 0012.242 5.072c4.43 0 8.861-1.689 12.242-5.072 6.764-6.76 6.764-17.726 0-24.484l-77.209-77.218h323.572c9.562 0 17.316-7.753 17.316-17.315 0-9.562-7.753-17.314-17.316-17.314z"
+                ></path>
+              </svg>
+            </button>
+            <button data-glide-dir=">">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 268.832 268.832"
+                class="right__arrow"
+              >
+                <path
+                  d="M265.171 125.577l-80-80c-4.881-4.881-12.797-4.881-17.678 0-4.882 4.882-4.882 12.796 0 17.678l58.661 58.661H12.5c-6.903 0-12.5 5.597-12.5 12.5 0 6.902 5.597 12.5 12.5 12.5h213.654l-58.659 58.661c-4.882 4.882-4.882 12.796 0 17.678 2.44 2.439 5.64 3.661 8.839 3.661s6.398-1.222 8.839-3.661l79.998-80c4.882-4.882 4.882-12.796 0-17.678z"
+                ></path>
+              </svg>
+            </button>
+          </template>
+        </vue-glide>
       </div>
     </section>
   </div>
@@ -163,12 +206,15 @@ import NewsCard from "@/components/NewsCard.vue";
 import TestimonialCard from "@/components/TestimonialCard.vue";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Glide, GlideSlide } from "vue-glide-js";
 
 export default {
   name: "Home",
   components: {
     NewsCard,
-    TestimonialCard
+    TestimonialCard,
+    [Glide.name]: Glide,
+    [GlideSlide.name]: GlideSlide
   },
   mounted() {
     const SCREEN_WIDTH = window.innerWidth,
