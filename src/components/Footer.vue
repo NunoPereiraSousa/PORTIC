c6a28cbd764e29ca1dddd626d93c8ad700ff7c56
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-15 15:37:04 +0000

    Footer done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,101 +1,116 @@
 <template>
-  <section class="footer grid">
-    <div class="footer__timezones">
-      <h1 class="footer__timezones__title title">Saturday, March 15th</h1>
-      <div class="footer__timezones__worldwide grid">
-        <div class="footer__timezones__times">
-          <h2>
-            12:10
-            <sup>AM</sup>
-          </h2>
-          <p>USA</p>
-        </div>
-        <div class="footer__timezones__times">
-          <h2>
-            01:12
-            <sup>AM</sup>
-          </h2>
-          <p>SINGAPORE</p>
-        </div>
-        <div class="footer__timezones__times">
-          <h2>
-            05:10
-            <sup>PM</sup>
-          </h2>
-          <p>LON</p>
-        </div>
-        <div class="footer__timezones__times">
-          <h2>
-            08:13
-            <sup>AM</sup>
-          </h2>
-          <p>QATAR</p>
-        </div>
-      </div>
-    </div>
-    <div class="footer__directions">
-      <h1 class="footer__directions__title title">How to get to PORTIC</h1>
-      <div class="footer__directions__info">
-        <p>Rua Arquitecto Lobão Vital, 172</p>
-        <p>4200-375 Porto</p>
-        <p>Portugal</p>
-        <p>(+351) 22 557 1020</p>
-        <p>portic@portic.ipp.pt</p>
-      </div>
-    </div>
-    <div class="footer__links">
-      <h1 class="footer__links__title title">Quick Links</h1>
-      <div class="footer__links__info">
-        <p>
-          <a href="">Areas of Actuation</a>
-        </p>
-        <p>
-          <a href="">Courses</a>
-        </p>
-        <p>
-          <a href="">Media</a>
-        </p>
-        <p>
-          <a href="">Positions</a>
-        </p>
-        <p>
-          <a href="">Projects</a>
-        </p>
-        <p>
-          <a href="">Unities</a>
-        </p>
-      </div>
-    </div>
-    <div class="footer__socials">
-      <h1 class="footer__socials__title title">Social Media</h1>
-      <div class="footer__socials__media">
-        <div class="grid">
-          <i class="fab fa-instagram"></i>
-          <p>Instagram</p>
-        </div>
-        <div class="grid">
-          <i class="fab fa-twitter"></i>
-          <p>Twitter</p>
-        </div>
-        <div class="grid">
-          <i class="fab fa-facebook-square"></i>
-          <p>Facebook</p>
-        </div>
-        <div class="grid">
-          <i class="fab fa-linkedin"></i>
-          <p>LinkedIn</p>
-        </div>
-        <div class="grid">
-          <i class="fab fa-youtube"></i>
-          <p>YouTube</p>
-        </div>
-        <div class="grid">
-          <i class="fab fa-flickr"></i>
-          <p>Flickr</p>
+  <div>
+    <section class="footer grid">
+      <div class="footer__timezones">
+        <h1 class="footer__timezones__title title">Saturday, March 15th</h1>
+        <div class="footer__timezones__worldwide grid">
+          <div class="footer__timezones__times">
+            <h2>
+              12:10
+              <sup>AM</sup>
+            </h2>
+            <p>USA</p>
+          </div>
+          <div class="footer__timezones__times">
+            <h2>
+              01:12
+              <sup>AM</sup>
+            </h2>
+            <p>SINGAPORE</p>
+          </div>
+          <div class="footer__timezones__times">
+            <h2>
+              05:10
+              <sup>PM</sup>
+            </h2>
+            <p>LON</p>
+          </div>
+          <div class="footer__timezones__times">
+            <h2>
+              08:13
+              <sup>AM</sup>
+            </h2>
+            <p>QATAR</p>
+          </div>
         </div>
       </div>
+      <div class="footer__directions">
+        <h1 class="footer__directions__title title">How to get to PORTIC</h1>
+        <div class="footer__directions__info">
+          <p>Rua Arquitecto Lobão Vital, 172</p>
+          <p>4200-375 Porto</p>
+          <p>Portugal</p>
+          <p>(+351) 22 557 1020</p>
+          <p>portic@portic.ipp.pt</p>
+        </div>
+      </div>
+      <div class="footer__links">
+        <h1 class="footer__links__title title">Quick Links</h1>
+        <div class="footer__links__info grid">
+          <p>
+            <a href="">Areas of Actuation</a>
+          </p>
+          <p>
+            <a href="">Courses</a>
+          </p>
+          <p>
+            <a href="">Media</a>
+          </p>
+          <p>
+            <a href="">Positions</a>
+          </p>
+          <p>
+            <a href="">Projects</a>
+          </p>
+          <p>
+            <a href="">Unities</a>
+          </p>
+        </div>
+      </div>
+      <div class="footer__socials">
+        <h1 class="footer__socials__title title">Social Media</h1>
+        <div class="footer__socials__media grid">
+          <div class="first__cells">
+            <a href="" class="grid">
+              <i class="fab fa-instagram"></i>
+              <p>Instagram</p>
+            </a>
+            <a href="" class="grid">
+              <i class="fab fa-twitter"></i>
+              <p>Twitter</p>
+            </a>
+            <a href="" class="grid">
+              <i class="fab fa-facebook-square"></i>
+              <p>Facebook</p>
+            </a>
+          </div>
+          <div class="second__cells">
+            <a href="" class="grid">
+              <i class="fab fa-linkedin"></i>
+              <p>LinkedIn</p>
+            </a>
+            <a href="" class="grid">
+              <i class="fab fa-youtube"></i>
+              <p>YouTube</p>
+            </a>
+            <a href="" class="grid">
+              <i class="fab fa-flickr"></i>
+              <p>Flickr</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="scroll_up">
+      <div class="flex flex-ai-c flex-jc-fe">
+        <p>Back to top</p>
+        <i class="fas fa-chevron-up"></i>
+      </div>
     </div>
-  </section>
+    <div class="finance__logos flex flex-ai-c flex-jc-c">
+      <img src="../../assets/financing-logos.png" alt="Finance Logos" />
+    </div>
+  </div>
 </template>
 
 <script>
