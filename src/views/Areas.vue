d601918e03b157f22d3cccdce76792c7531c8429
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-16 00:37:08 +0000

    Time and date working

---
+++ src/views/Areas.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="areas">
+    <div class="scene"></div>
+  </div>
+</template>
+
+<script>
+import { background } from "../../js/background";
+export default {
+  mounted() {
+    background();
+  }
+};
+</script>
