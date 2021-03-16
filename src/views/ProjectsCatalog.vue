6e87241f2e546a755f27fd66e15732b80e85d6db
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-16 23:09:05 +0000

    Projects catalog UI completed

--- src/views/ProjectsCatalog.vue
+++ src/views/ProjectsCatalog.vue
@@ -3,7 +3,9 @@
     <div class="flex flex-jc-c flex-ai-c flex-fd-c">
       <h3>PORTIC <span>&#9654;</span> Projects</h3>
       <h1 class="projects_catalog__title">Projects</h1>
-      <p>These are the projects that our team was part of.</p>
+      <p class="projects_catalog__quote">
+        These are the projects that our team was part of.
+      </p>
     </div>
     <div class="projects_catalog__filters grid">
       <input type="text" id="projectTxt" placeholder="Search project" />
@@ -20,44 +22,51 @@
         </h2>
       </div>
       <div class="hide-for-mobile">
-        <h2 class="projects_catalog__filters__sorting">Finance sorting</h2>
-        <h2
+        <button class="projects_catalog__filters__sorting">
+          Finance sorting
+        </button>
+      </div>
+      <div class="hide-for-mobile" style="justify-self: start;">
+        <button
           class="projects_catalog__filters__institutions"
           @click="showFilters"
         >
           Institutions
-        </h2>
-      </div>
-      <div class="projects_catalog__filters__categories">
-        <div class="grid">
-          <p>Lorem</p>
-          <p>Lorem</p>
-          <p>Lorem</p>
-          <p>Lorem</p>
-          <p>Lorem</p>
-          <p>Lorem</p>
-          <p>Lorem</p>
-          <p>Lorem</p>
-        </div>
-      </div>
-      <div class="projects__grid grid" style="width: 100%">
-        <ProjectCard
-          v-for="i in 12"
-          :key="i"
-          :counter="i"
-          @mouseover.native="onHover"
-          @mouseleave.native="notHover"
-        />
+        </button>
       </div>
     </div>
+    <div class="projects_catalog__filters__categories">
+      <div class="grid">
+        <p>Lorem</p>
+        <p>Lorem</p>
+        <p>Lorem</p>
+        <p>Lorem</p>
+        <p>Lorem</p>
+        <p>Lorem</p>
+        <p>Lorem</p>
+        <p>Lorem</p>
+      </div>
+    </div>
+    <div class="projects__grid grid" style="width: 100%">
+      <ProjectCard
+        v-for="i in 12"
+        :key="i"
+        :counter="i"
+        @mouseover.native="onHover"
+        @mouseleave.native="notHover"
+      />
+    </div>
+    <Footer />
   </section>
 </template>
 
 <script>
 import ProjectCard from "@/components/ProjectCard.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   components: {
-    ProjectCard
+    ProjectCard,
+    Footer
   },
   mounted() {},
   methods: {
@@ -65,6 +74,16 @@ export default {
       document
         .querySelector(".projects_catalog__filters__categories")
         .classList.toggle("opened");
+    },
+    onHover(element) {
+      this.selectedItem = element.target;
+
+      this.selectedItem.classList.toggle("hovered");
+    },
+    notHover(element) {
+      this.selectedItem = element.target;
+
+      this.selectedItem.classList.remove("hovered");
     }
   }
 };
