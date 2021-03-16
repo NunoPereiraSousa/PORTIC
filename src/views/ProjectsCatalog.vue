9e21713529c3e9ddc1ff6afd27a3d8b1bbc73351
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-16 19:06:36 +0000

    Project catalog in progress

--- src/views/ProjectsCatalog.vue
+++ src/views/ProjectsCatalog.vue
@@ -2,7 +2,7 @@
   <section class="projects_catalog">
     <div class="flex flex-jc-c flex-ai-c flex-fd-c">
       <h3>PORTIC <span>&#9654;</span> Projects</h3>
-      <h1>Projects</h1>
+      <h1 class="projects_catalog__title">Projects</h1>
       <p>These are the projects that our team was part of.</p>
     </div>
     <div class="projects_catalog__filters grid">
@@ -12,16 +12,60 @@
         style="width: 100%"
       >
         <h2 class="projects_catalog__filters__sorting">Finance sorting</h2>
-        <h2 class="projects_catalog__filters__institutions">Institutions</h2>
+        <h2
+          class="projects_catalog__filters__institutions"
+          @click="showFilters"
+        >
+          Institutions
+        </h2>
       </div>
       <div class="hide-for-mobile">
         <h2 class="projects_catalog__filters__sorting">Finance sorting</h2>
-        <h2 class="projects_catalog__filters__institutions">Institutions</h2>
+        <h2
+          class="projects_catalog__filters__institutions"
+          @click="showFilters"
+        >
+          Institutions
+        </h2>
+      </div>
+      <div class="projects_catalog__filters__categories">
+        <div class="grid">
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+        </div>
+      </div>
+      <div class="projects__grid grid" style="width: 100%">
+        <ProjectCard
+          v-for="i in 12"
+          :key="i"
+          :counter="i"
+          @mouseover.native="onHover"
+          @mouseleave.native="notHover"
+        />
       </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+import ProjectCard from "@/components/ProjectCard.vue";
+export default {
+  components: {
+    ProjectCard
+  },
+  mounted() {},
+  methods: {
+    showFilters() {
+      document
+        .querySelector(".projects_catalog__filters__categories")
+        .classList.toggle("opened");
+    }
+  }
+};
 </script>
