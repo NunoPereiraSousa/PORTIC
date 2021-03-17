cc4feb8050a45071b83cf0f64b29202489c169ab
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-17 00:21:47 +0000

    Projects landing UI updated

--- src/views/ProjectsCatalog.vue
+++ src/views/ProjectsCatalog.vue
@@ -1,65 +1,67 @@
 <template>
-  <section class="projects_catalog">
-    <div class="flex flex-jc-c flex-ai-c flex-fd-c">
-      <h3>PORTIC <span>&#9654;</span> Projects</h3>
-      <h1 class="projects_catalog__title">Projects</h1>
-      <p class="projects_catalog__quote">
-        These are the projects that our team was part of.
-      </p>
-    </div>
-    <div class="projects_catalog__filters grid">
-      <input type="text" id="projectTxt" placeholder="Search project" />
-      <div
-        class="flex flex-jc-sa flex-ai-c hide-for-desktop"
-        style="width: 100%"
-      >
-        <button class="projects_catalog__filters__sorting">
-          Finance sorting
-        </button>
-        <button
-          class="projects_catalog__filters__institutions"
-          @click="showFilters"
+  <div>
+    <section class="projects_catalog">
+      <div class="flex flex-jc-c flex-ai-c flex-fd-c">
+        <h3>PORTIC <span>&#9654;</span> Projects</h3>
+        <h1 class="projects_catalog__title">Projects</h1>
+        <p class="projects_catalog__quote">
+          These are the projects that our team was part of.
+        </p>
+      </div>
+      <div class="projects_catalog__filters grid">
+        <input type="text" id="projectTxt" placeholder="Search project" />
+        <div
+          class="flex flex-jc-sa flex-ai-c hide-for-desktop"
+          style="width: 100%"
         >
-          Institutions
-        </button>
+          <button class="projects_catalog__filters__sorting">
+            Finance sorting
+          </button>
+          <button
+            class="projects_catalog__filters__institutions"
+            @click="showFilters"
+          >
+            Institutions
+          </button>
+        </div>
+        <div class="hide-for-mobile">
+          <button class="projects_catalog__filters__sorting">
+            Finance sorting
+          </button>
+        </div>
+        <div class="hide-for-mobile" style="justify-self: start;">
+          <button
+            class="projects_catalog__filters__institutions"
+            @click="showFilters"
+          >
+            Institutions
+          </button>
+        </div>
       </div>
-      <div class="hide-for-mobile">
-        <button class="projects_catalog__filters__sorting">
-          Finance sorting
-        </button>
+      <div class="projects_catalog__filters__categories">
+        <div class="grid">
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+          <p>Lorem</p>
+        </div>
       </div>
-      <div class="hide-for-mobile" style="justify-self: start;">
-        <button
-          class="projects_catalog__filters__institutions"
-          @click="showFilters"
-        >
-          Institutions
-        </button>
+      <div class="projects__grid grid" style="width: 100%">
+        <ProjectCard
+          v-for="i in 12"
+          :key="i"
+          :counter="i"
+          @mouseover.native="onHover"
+          @mouseleave.native="notHover"
+        />
       </div>
-    </div>
-    <div class="projects_catalog__filters__categories">
-      <div class="grid">
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-      </div>
-    </div>
-    <div class="projects__grid grid" style="width: 100%">
-      <ProjectCard
-        v-for="i in 12"
-        :key="i"
-        :counter="i"
-        @mouseover.native="onHover"
-        @mouseleave.native="notHover"
-      />
-    </div>
+    </section>
     <Footer />
-  </section>
+  </div>
 </template>
 
 <script>
