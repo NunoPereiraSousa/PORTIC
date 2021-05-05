ee00c73a3b010fafddf496b7ef75bc745ae7ec28
Nuno Sousa <9180579@esmad.ipp.pt>
2021-05-05 17:22:23 +0100

    Areas popup working and getting the correct area

---
+++ src/components/Dashboard/DashboardAreasPopup.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="admin_areas__popup admin_delete_popup">
+    <h2>Tem a certeza que pretende eliminar a área:</h2>
+    <p>{{ areaName }}</p>
+
+    <div class="flex flex-ai-c">
+      <button class="admin_delete_popup__confirm">Confimar</button>
+      <button class="admin_delete_popup__cancel" @click="closePopup">
+        Cancelar
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "DashboardAreasPopup",
+  props: {
+    areaName: {
+      type: String,
+      required: false
+    }
+  },
+  computed: {
+    ...mapGetters(["getSelectedAreaByID", "getAreaByID"])
+  },
+  methods: {
+    closePopup() {
+      let admin_areas__panel__overlay = document.querySelector(
+        ".admin_areas__panel__overlay"
+      );
+
+      let admin_delete_popup = document.querySelector(".admin_delete_popup");
+
+      admin_areas__panel__overlay.classList.toggle("show_overlay");
+      admin_delete_popup.classList.toggle("show_popup");
+    }
+  }
+};
+</script>
