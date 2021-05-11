941dd9d5e9161b0730fdfbf47c2bdd6ffc277109
Nuno Sousa <9180579@esmad.ipp.pt>
2021-05-11 12:23:55 +0100

    Landing and header UI updates

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -16,7 +16,9 @@
         Technology & Innovation Center
       </div>
     </h1>
-    <p class="quote__paragraph">
+
+    <div class="quote__img"></div>
+    <!-- <p class="quote__paragraph">
       <span>O PORTIC -Porto Research, Technology & Innovation Center </span>visa
       agregar vários centros e grupos de investigação das escolas do P.PORTO num
       único espaço físico, configurando uma superestrutura dedicada à
@@ -24,7 +26,7 @@
       Alojará ainda a Porto Global Hub que integra a Porto Design Factory, a
       Porto Business Innovation e a Startup Porto e que tem como visão ajudar a
       criação de projetos locais sustentáveis para uma vida melhor.
-    </p>
+    </p> -->
   </section>
 </template>
 
