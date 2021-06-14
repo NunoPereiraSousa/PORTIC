406452b9909d4c342c0376a73119f8e128cf73e5
Nuno Pereira Sousa <9180579@esmad.ipp.pt>
2021-06-14 16:11:38 +0100

    Careers page UI changed

--- src/views/Positions.vue
+++ src/views/Positions.vue
@@ -1,46 +1,7 @@
 <template>
   <div class="positions">
-    <!-- <div class="scene"></div> -->
-    <!-- <Intro extra="our " keyword="Projects" /> -->
-    <SubPageIntro
-      :categoryTitle="$t('careers.careersKey')"
-      :title1="$t('careers.nCareers', { n: $store.getters.getCareersLength })"
-      :text1="$t('careers.nCareersDesc')"
-      :title2="$t('careers.careersTitle2')"
-      :text2="$t('careers.careersTitle2Desc')"
-      content="Research and development, technology and knowledge transfer, innovation and creativity, entrepreneurship, incubation, spin-offs, startups – these are all part of Research, Technology & Innovation, a holistic chain of interrelated activities.
-PORTIC includes units and groups with activities in different stages of the knowledge and innovation chain, in several areas of knowledge."
-      :categoryDescription="$t('careers.infoDesc')"
-    />
-
-    <section class="positions__find_program">
-      <h1 class="positions__find_program__title">
-        {{ $t("careers.mainTitle") }}
-      </h1>
-
-      <SubHeaderTitle :text="$t('careers.title')" />
-
-      <p class="positions__find_program__info">
-        {{ $t("careers.desc") }}
-      </p>
-    </section>
-
-    <section class="positions__come_prepared">
-      <SubHeaderTitle :text="$t('careers.title2')" class="light" />
-
-      <div class="positions__come_prepared__grid grid">
-        <ComePreparedCard
-          v-for="(tip, index) in getTips"
-          :key="tip.id_hiring_tip"
-          :counter="index + 1"
-          :title="tip.title"
-          :desc="tip.description"
-        />
-      </div>
-    </section>
-
     <section class="positions__available">
-      <SubHeaderTitle :text="$t('careers.title3')" />
+      <!-- <SubHeaderTitle :text="$t('careers.title3')" /> -->
       <div class="positions__available__grid grid">
         <CareersCard
           v-for="(position, index) in $store.getters.getCareers"
@@ -61,17 +22,11 @@ PORTIC includes units and groups with activities in different stages of the know
 <script>
 import { mapGetters } from "vuex";
 
-import SubPageIntro from "@/components/SubPageIntro.vue";
-import SubHeaderTitle from "@/components/SubHeaderTitle.vue";
-import ComePreparedCard from "@/components/Positions/ComePreparedCard.vue";
 import CareersCard from "@/components/Positions/CareersCard.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
-    SubPageIntro,
-    SubHeaderTitle,
-    ComePreparedCard,
     CareersCard,
     Footer
   },
